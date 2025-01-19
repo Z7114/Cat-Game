e42e7ab0fe02562bb42f3a963a87ed8fed50c786
Z7114 <zynadainkeh@gmail.com>
2025-01-19 21:12:40 +0000

    added ml5

--- sketch.js
+++ sketch.js
@@ -1,10 +1,24 @@
 
+// visuals
 let catPoses = [];
+let display;
+let pose = 2;
+
+// movement
+let video;
+let handPose;
+let hands = []; // keeps track of the poses detected
+let connections;
 let screen;
+
+
+// audio
 let mic;
-let pose = 0;
+
+
 
 function preload() {
+  // loading illustrations
   catPoses[0] = loadImage('assets/Afraid.png');
   catPoses[1] = loadImage('assets/Attacking.png');
   catPoses[2] = loadImage('assets/Calm.png');
@@ -19,49 +33,91 @@ function preload() {
   catPoses[11] = loadImage('assets/On Guard R.png');
   catPoses[12] = loadImage('assets/Trusting.png');
 
-  screen = loadImage('assets/Main Screen.png');
+  display = loadImage('assets/Main Screen.png');
+
+  // load the hand pose model
+  handPose = ml5.handPose();
 
 
+}
+
+function mousePressed() {
+  console.log(hands);
 }
 
 
 function setup() {
   // uploading game screen
   createCanvas(750, 750);
-  image(screen, 0, 0, 750, 750);
+  image(display, 0, 0, 750, 750);
+  screen = createGraphics(640, 480);
+
+  // create the webcam video and hide it
+  video = createCapture(VIDEO);
+  video.size(640, 480);
+  video.hide();
+
+  // start detecting hands from webcam
+  handPose.detectStart(video, gotHands);
+
+  // get the skeletal connections
+  connections = handPose.getConnections();
 
   // set up mic
   mic = new p5.AudioIn();
   mic.start();
   getAudioContext().suspend();
 
+
 }
 
 
 function draw() {
+  image(display, 0, 0, 750, 750);
 
-  cat();
-
-  
-  // if (mouseIsPressed) {
-  //   image(catPoses[2], 100, 100, 500, 500);
+  // if (pose >= 0) {
+  //   image(catPoses[pose], 170, 250, 400, 400);
   // }
+
+  for (let i = 0; i < hands.length; i ++) {
+    let hand = hands[i];
+  // defining individual points
+  let index = hand.index_finger_tip;
+  let thumb = hand.thumb_tip;
+
+  // getting distance between points
+  let d = dist(index.x, index.y, thumb.x, thumb.y);
+  // condition for drawing to display based on keypoint distance
+  if (d < 30) {
+    image(catPoses[6], 130, 250, 500, 500);
+  }
+}
 }
 
 
-function mousePressed() {
-  mic.start();
-  getAudioContext().resume();
+// function mousePressed() {
+//   mic.start();
+//   getAudioContext().resume();
+// }
+
+
+// function cat() {
+//   let vol = mic.getLevel(); // get audio data from mic
+//   if (vol > 0.5) {
+//     image(catPoses[9], 130, 250, 500, 500);
+//     // console.log("Too loud");
+
+//    }
+// }
+
+function keyTyped() {
+  if (keyCode === 32) {
+    pose = 0;
+  }  
 }
 
-
-function cat() {
-  let vol = mic.getLevel(); // get audio data from mic
-
-  if (vol > 0.5) {
-    image(catPoses[9], 130, 250, 500, 500);
-    console.log("Too loud");
-   } else {
-    
-   }
+// callback function for when handPose outputs data
+function gotHands(results) {
+  // save the output to the hands variable
+  hands = results;
 }
