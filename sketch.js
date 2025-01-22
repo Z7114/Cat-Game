fb9e7c0f1d33b12bedd0c4712a06375b9d6e6ea4
Z7114 <zynadainkeh@gmail.com>
2025-01-22 18:04:20 +0000

    not the arduino

--- sketch.js
+++ sketch.js
@@ -18,7 +18,10 @@ let mic;
 // arduino
 let port;
 let connectBtn;
-let serial;
+let str;
+let numInputs = 2;
+let values = [];
+let a, b;
 
 // input states ??
 let gesture;
@@ -26,7 +29,7 @@ let noise;
 let light;
 let touch;
 
-// storing input data
+// storing input data (with Agnes' help XD)
 let catState = [gesture, noise, light, touch];
 
 
@@ -90,9 +93,45 @@ function draw() {
   image(display, 0, 0, 750, 750);
   // remember to add background changes with hiding pose + low light detected
 
+  // all Agnes ^^
   checkCatState(); // using function to check for input states
 
+  // if (catState[0] == 0 && catState[1] == 0 && catState[2] == 0 && catState[3] == 0) {
+  //   selectedPose = 2; // night mode
+  // } else if (catState[0] == 0 && catState[1] == 0 && catState[2] == 0 && catState[3] == 1) {
+  //   selectedPose = 8; // night mode (NM)
+  // } else if (catState[0] == 0 && catState[1] == 0 && catState[2] == 1 && catState[3] == 0) {
+  //   selectedPose = 8; // NM
+  // } else if (catState[0] == 0 && catState[1] == 0 && catState[2] == 1 && catState[3] == 1) {
+  //   selectedPose = 1; // NM
+  // } else if (catState[0] == 0 && catState[1] == 1 && catState[2] == 0 && catState[3] == 0) {
+  //   selectedPose = 0; // NM
+  // } else if (catState[0] == 0 && catState[1] == 1 && catState[2] == 0 && catState[3] == 1) {
+  //   selectedPose = 7; // different background, NM
+  // } else if (catState[0] == 0 && catState[1] == 1 && catState[2] == 1 && catState[3] == 0) {
+  //   selectedPose = 6; // NM
+  // } else if (catState[0] == 0 && catState[1] == 1 && catState[2] == 1 && catState[3] == 1) {
+  //   selectedPose = 6; // NM
+  // } else if (catState[0] == 1 && catState[1] == 0 && catState[2] == 0 && catState[3] == 0) {
+  //   selectedPose = 5; // resize image
+  // } else if (catState[0] == 1 && catState[1] == 0 && catState[2] == 0 && catState[3] == 1) {
+  //   selectedPose = 0; //
+  // } else if (catState[0] == 1 && catState[1] == 0 && catState[2] == 1 && catState[3] == 0) {
+  //   selectedPose = 9; //
+  // } else if (catState[0] == 1 && catState[1] == 0 && catState[2] == 1 && catState[3] == 1) {
+  //   selectedPose = 1; //
+  // } else if (catState[0] == 1 && catState[1] == 1 && catState[2] == 0 && catState[3] == 0) {
+  //   selectedPose = 0; //
+  // } else if (catState[0] == 1 && catState[1] == 1 && catState[2] == 0 && catState[3] == 1) {
+  //   selectedPose = 4; //
+  // } else if (catState[0] == 1 && catState[1] == 1 && catState[2] == 1 && catState[3] == 0) {
+  //   selectedPose = 7; // different bg
+  // } else if (catState[0] == 1 && catState[1] == 1 && catState[2] == 1 && catState[3] == 1) {
+  //   selectedPose = 1; //
+  // }
 
+
+  // this is wrong! it's calling the variable in the array, not it's value!!
   if (catState[0, 0, 0, 0]) {
     selectedPose = 2; // night mode
   } else if (catState[0, 0, 0, 1]) {
@@ -150,7 +189,7 @@ function draw() {
   // // console.log(wrist.x, wrist.y);
 
   // // console.log(d, z);
-  // // Store selected image
+  // // Store selected image (help from Will + Irti)
 
   // // condition for drawing to display based on keypoint distance
   //   if (d < 20 && z > 20) {
@@ -184,11 +223,6 @@ function gotHands(results) {
   hands = results;
 }
 
-// function mousePressed() {
-//   mic.start();
-//   getAudioContext().resume();
-// }
-
 
 // connect arduino button
 function connectBtnClick() {
@@ -219,28 +253,44 @@ function checkCatState() {
 
   // checking noise state // WORKS
   let vol = mic.getLevel(); // get audio data from mic
-  if (vol < 0.5) { // checking for loud or quiet sound
+  if (vol < 0.3) { // checking for loud or quiet sound
     catState[1] = 0;
   } else {
     catState[1] = 1;
   }
   // console.log(vol);
 
-  // checking light state 
-  let ldrValue = port.readUntil("\n"); // read port data... until new line("\n")?
-  if(ldrValue < 500) {
-    catState[2] = 0;
-  } else {
-    catState[2] = 1;
-  }
-  console.log(ldrValue);
+  // read port data
+  let str = port.readUntil("\n");
 
-  // checking touch state
-  let sensorValue = port.readUntil("\n");
-  if(sensorValue == 626) {
-    catState[3] = 0;
-  } else {
-    catState[3] = 1;
-  }
+  // getting p5 to read both inputs from arduino (with Mia's help ^^)
+  if (str) {
+    values = int(split(str, ","));
+    let data = values;
+    // console.log("Values: " + values);
+  
+  for (let i = 0; i < values; i++) {
+      a = data[0];
+      b = data[1];
 
+      console.log(a);
+      console.log(b);
+
+      if (a < 600) {
+        catState[2] = 0;
+      } else {
+        catState[2] = 1;
+      }
+    
+      // checking touch state
+      if (b == 0) {
+        catState[3] = 0;
+      } else {
+        catState[3] = 1;
+      }
+      
+    }
+  }
 }
+
+// return(catState);
